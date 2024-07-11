48b7f34994cc0fc5754d4d91c61e38f641011b9d
Angelo Moura <angelomoura@gmail.com>
2024-07-11 08:42:17 -0300

    Include a routine to get inputs

--- main.py
+++ main.py
@@ -1,102 +1,19 @@
 from siemens_lib.s7_connection import S7Connection
+from siemens_lib.s7_get_inputs import S7GetInput
+
 
 def main(args):
     print(f'Hello world! {args}')
-    MEB = S7Connection(ip='192.168.0.55', rack=0, cpu_slot=4)
 
-    # Read MB test
-    print(f'Read MB5 test')
-    # print(f'Leitura de MB bit 0 {MEB.mb_read(mb = 5, bit = 0, type = 0)}')
-    # print(f'Leitura de MB bit 1 {MEB.mb_read(mb = 5, bit = 1, type = 0)}')
-    # print(f'Leitura de MB bit 2 {MEB.mb_read(mb = 5, bit = 2, type = 0)}')
-    # print(f'Leitura de MB bit 3 {MEB.mb_read(mb = 5, bit = 3, type = 0)}')
-    # print(f'Leitura de MB bit 4 {MEB.mb_read(mb = 5, bit = 4, type = 0)}')
-    # print(f'Leitura de MB bit 5 {MEB.mb_read(mb = 5, bit = 5, type = 0)}')
-    # print(f'Leitura de MB bit 6 {MEB.mb_read(mb = 5, bit = 6, type = 0)}')
-    # print(f'Leitura de MB bit 7 {MEB.mb_read(mb = 5, bit = 7, type = 0)}')
-
-    print(f'Leitura do byte em binário {MEB.mb_read(mb = 5, type = 1)}')
-
-    # print(f'Leitura do byte em inteiro {MEB.mb_read(mb = 5, type = 2)}')
-    # print(f'Leitura do byte em char {MEB.mb_read(mb = 5, type = 3)}')
-
-    print(f'Read MB6 test')
-    # print(f'Leitura de MB bit 0 {MEB.mb_read(mb = 6, bit = 0, type = 0)}')
-    # print(f'Leitura de MB bit 1 {MEB.mb_read(mb = 6, bit = 1, type = 0)}')
-    # print(f'Leitura de MB bit 2 {MEB.mb_read(mb = 6, bit = 2, type = 0)}')
-    # print(f'Leitura de MB bit 3 {MEB.mb_read(mb = 6, bit = 3, type = 0)}')
-    # print(f'Leitura de MB bit 4 {MEB.mb_read(mb = 6, bit = 4, type = 0)}')
-    # print(f'Leitura de MB bit 5 {MEB.mb_read(mb = 6, bit = 5, type = 0)}')
-    # print(f'Leitura de MB bit 6 {MEB.mb_read(mb = 6, bit = 6, type = 0)}')
-    # print(f'Leitura de MB bit 7 {MEB.mb_read(mb = 6, bit = 7, type = 0)}')
-
-    print(f'Leitura do byte em binário {MEB.mb_read(mb = 6, type = 1)}')
-    print(f'Leitura do byte em inteiro {MEB.mb_read(mb = 6, type = 2)}')
-    print(f'Leitura do byte em char {MEB.mb_read(mb = 6, type = 3)}')
-
-    # Read MW test
-    print(f'Read MW5 test')
-    # print(f'Leitura de MW bit 0 {MEB.mw_read(mw=5, bit=0, type=0)}')
-    # print(f'Leitura de MW bit 1 {MEB.mw_read(mw=5, bit=1, type=0)}')
-    # print(f'Leitura de MW bit 2 {MEB.mw_read(mw=5, bit=2, type=0)}')
-    # print(f'Leitura de MW bit 3 {MEB.mw_read(mw=5, bit=3, type=0)}')
-    # print(f'Leitura de MW bit 4 {MEB.mw_read(mw=5, bit=4, type=0)}')
-    # print(f'Leitura de MW bit 5 {MEB.mw_read(mw=5, bit=5, type=0)}')
-    # print(f'Leitura de MW bit 6 {MEB.mw_read(mw=5, bit=6, type=0)}')
-    # print(f'Leitura de MW bit 7 {MEB.mw_read(mw=5, bit=7, type=0)}')
-    # print(f'Leitura de MW bit 8 {MEB.mw_read(mw=5, bit=8, type=0)}')
-    # print(f'Leitura de MW bit 9 {MEB.mw_read(mw=5, bit=9, type=0)}')
-    # print(f'Leitura de MW bit 10 {MEB.mw_read(mw=5, bit=10, type=0)}')
-    # print(f'Leitura de MW bit 11 {MEB.mw_read(mw=5, bit=11, type=0)}')
-    # print(f'Leitura de MW bit 12 {MEB.mw_read(mw=5, bit=12, type=0)}')
-    # print(f'Leitura de MW bit 13 {MEB.mw_read(mw=5, bit=13, type=0)}')
-    # print(f'Leitura de MW bit 14 {MEB.mw_read(mw=5, bit=14, type=0)}')
-    # print(f'Leitura de MW bit 15 {MEB.mw_read(mw=5, bit=15, type=0)}')
-
-    print(f'Leitura da word em binário {MEB.mw_read(mw = 5, type = 1)}')
-    print(f'Leitura da word em inteiro {MEB.mw_read(mw = 5, type = 2)}')
-    print(f'Leitura do byte em char {MEB.mw_read(mw = 5, type = 3)}')
-
-    # Read MD test
-    print(f'Read MD5 test')
-    # print(f'Leitura de MD bit 0 {MEB.md_read(md=5, bit=0, type=0)}')
-    # print(f'Leitura de MD bit 1 {MEB.md_read(md=5, bit=1, type=0)}')
-    # print(f'Leitura de MD bit 2 {MEB.md_read(md=5, bit=2, type=0)}')
-    # print(f'Leitura de MD bit 3 {MEB.md_read(md=5, bit=3, type=0)}')
-    # print(f'Leitura de MD bit 4 {MEB.md_read(md=5, bit=4, type=0)}')
-    # print(f'Leitura de MD bit 5 {MEB.md_read(md=5, bit=5, type=0)}')
-    # print(f'Leitura de MD bit 6 {MEB.md_read(md=5, bit=6, type=0)}')
-    # print(f'Leitura de MD bit 7 {MEB.md_read(md=5, bit=7, type=0)}')
-    # print(f'Leitura de MD bit 8 {MEB.md_read(md=5, bit=8, type=0)}')
-    # print(f'Leitura de MD bit 9 {MEB.md_read(md=5, bit=9, type=0)}')
-    # print(f'Leitura de MD bit 10 {MEB.md_read(md=5, bit=10, type=0)}')
-    # print(f'Leitura de MD bit 11 {MEB.md_read(md=5, bit=11, type=0)}')
-    # print(f'Leitura de MD bit 12 {MEB.md_read(md=5, bit=12, type=0)}')
-    # print(f'Leitura de MD bit 13 {MEB.md_read(md=5, bit=13, type=0)}')
-    # print(f'Leitura de MD bit 14 {MEB.md_read(md=5, bit=14, type=0)}')
-    # print(f'Leitura de MD bit 15 {MEB.md_read(md=5, bit=15, type=0)}')
-    # print(f'Leitura de MD bit 16 {MEB.md_read(md=5, bit=16, type=0)}')
-    # print(f'Leitura de MD bit 17 {MEB.md_read(md=5, bit=17, type=0)}')
-    # print(f'Leitura de MD bit 18 {MEB.md_read(md=5, bit=18, type=0)}')
-    # print(f'Leitura de MD bit 19 {MEB.md_read(md=5, bit=19, type=0)}')
-    # print(f'Leitura de MD bit 20 {MEB.md_read(md=5, bit=20, type=0)}')
-    # print(f'Leitura de MD bit 21 {MEB.md_read(md=5, bit=21, type=0)}')
-    # print(f'Leitura de MD bit 22 {MEB.md_read(md=5, bit=22, type=0)}')
-    # print(f'Leitura de MD bit 23 {MEB.md_read(md=5, bit=23, type=0)}')
-    # print(f'Leitura de MD bit 24 {MEB.md_read(md=5, bit=24, type=0)}')
-    # print(f'Leitura de MD bit 25 {MEB.md_read(md=5, bit=25, type=0)}')
-    # print(f'Leitura de MD bit 26 {MEB.md_read(md=5, bit=26, type=0)}')
-    # print(f'Leitura de MD bit 27 {MEB.md_read(md=5, bit=27, type=0)}')
-    # print(f'Leitura de MD bit 28 {MEB.md_read(md=5, bit=28, type=0)}')
-    # print(f'Leitura de MD bit 29 {MEB.md_read(md=5, bit=29, type=0)}')
-    # print(f'Leitura de MD bit 30 {MEB.md_read(md=5, bit=30, type=0)}')
-    # print(f'Leitura de MD bit 31 {MEB.md_read(md=5, bit=31, type=0)}')
-
-    print(f'Leitura da word em binário {MEB.md_read(md = 5, type = 1)}')
-    print(f'Leitura da word em inteiro {MEB.md_read(md = 5, type = 2)}')
-    print(f'Leitura do byte em char {MEB.md_read(md = 5, type = 3)}')
+    ip = '192.168.0.55'
+    rack = 0
+    cpu_slot = 4
 
     
+    MEB = S7Connection(ip=ip, rack=rack, cpu_slot=cpu_slot)
+    MEB_inputs = S7GetInput(PLC = MEB)
+    MEB_inputs.getinput()
+
 
 if __name__ == '__main__':
     main('init')
--- siemens_lib/s7_connection.py
+++ siemens_lib/s7_connection.py
@@ -3,8 +3,8 @@ from snap7 import client
 
 '''
     Class to connect Siemens S7 connection. Also provides variables getters and setters
-    @AngeloMoura
-    072024
+    @Angelo Moura
+    julho 2024
 '''
 
 class S7Connection:
@@ -38,37 +38,37 @@ class S7Connection:
             self.__connection.connect(ip, rack, cpu_slot)
             self.__connection.get_connected()
             print('connected successfully')
-            print(type(self.__connection))
             return self.__connection
         except Exception as __exception:
             print(f'Connection failed. {__exception}')
+            return 'error connecting to S7'
 
     def __convert_bin(self, raw_value, length):
-            '''
-                Perform conversion from raw integer value to binary representation.
-                @raw_value_byte - integer value to convert
-                @length - size of array to convert (8, 16...)
-            '''
-            self.__raw_value_binary = bin(raw_value)
-            if raw_value < 0:
-                self.__raw_value_binary = self.__raw_value_binary[3:]
-                self.__changed_bits = str('')
-                for i in range(len(self.__raw_value_binary)):
-                    if self.__raw_value_binary[i] == '0':
-                        self.__changed_bits += '1'
-                    else:
-                        self.__changed_bits += '0'
-                add_zeros = length - len(self.__raw_value_binary)
-                self.__changed_bits = add_zeros * '1' + self.__changed_bits
-                self.__final_inverted_value = int(self.__changed_bits, 2) + 1
-                self.__binary = bin(self.__final_inverted_value)
-            else:
-                self.__raw_value_binary = self.__raw_value_binary[2:]
-                self.__raw_binary_len = len(self.__raw_value_binary)
-                self.__byte_add_len = length - self.__raw_binary_len
-                self.__binary = self.__byte_add_len * '0' + self.__raw_value_binary
-                self.__binary = '0b' + self.__binary
-            return self.__binary
+        '''
+            Perform conversion from raw integer value to binary representation.
+            @raw_value_byte - integer value to convert
+            @length - size of array to convert (8, 16...)
+        '''
+        self.__raw_value_binary = bin(raw_value)
+        if raw_value < 0:
+            self.__raw_value_binary = self.__raw_value_binary[3:]
+            self.__changed_bits = str('')
+            for i in range(len(self.__raw_value_binary)):
+                if self.__raw_value_binary[i] == '0':
+                    self.__changed_bits += '1'
+                else:
+                    self.__changed_bits += '0'
+            add_zeros = length - len(self.__raw_value_binary)
+            self.__changed_bits = add_zeros * '1' + self.__changed_bits
+            self.__final_inverted_value = int(self.__changed_bits, 2) + 1
+            self.__binary = bin(self.__final_inverted_value)
+        else:
+            self.__raw_value_binary = self.__raw_value_binary[2:]
+            self.__raw_binary_len = len(self.__raw_value_binary)
+            self.__byte_add_len = length - self.__raw_binary_len
+            self.__binary = self.__byte_add_len * '0' + self.__raw_value_binary
+            self.__binary = '0b' + self.__binary
+        return self.__binary
 
     def mb_read(self, mb, bit=0, type=1):
         '''
@@ -139,44 +139,45 @@ class S7Connection:
         return self.__word
 
     def md_read(self, md, bit=0, type=1):
-            '''
-                Perform return getter from MD. The value returned depends of type
-                    you entered.
-                    0 = Return bit from mb representation (consider as string)
-                    1 = Return word binary representation (consider as string)
-                    2 = Return word decimal representation (consider as int)
-                    3 = Return char representation (consider string)
-            '''
-            self.__DWORD_LEN = 32
-            self.__raw_value_word = int.from_bytes(self.__connection.mb_read(
-                md, 4), signed=True)
-            self.__binary = self.__convert_bin(
-                raw_value=self.__raw_value_word, length=self.__DWORD_LEN)
+        '''
+            Perform return getter from MD. The value returned depends of type
+                you entered.
+                0 = Return bit from mb representation (consider as string)
+                1 = Return word binary representation (consider as string)
+                2 = Return word decimal representation (consider as int)
+                3 = Return char representation (consider string)
+        '''
+        self.__DWORD_LEN = 32
+        self.__raw_value_word = int.from_bytes(self.__connection.mb_read(
+            md, 4), signed=True)
+        self.__binary = self.__convert_bin(
+            raw_value=self.__raw_value_word, length=self.__DWORD_LEN)
 
-            if type == 0:
-                self.__binary = self.__binary[2:]
-                self.__binary = self.__binary[self.__DWORD_LEN - 1 - bit]
-                self.__dword = self.__binary
+        if type == 0:
+            self.__binary = self.__binary[2:]
+            self.__binary = self.__binary[self.__DWORD_LEN - 1 - bit]
+            self.__dword = self.__binary
 
-            if type == 1:
-                self.__dword = self.__binary
+        if type == 1:
+            self.__dword = self.__binary
 
-            if type == 2:
-                self.__dword = self.__raw_value_word
+        if type == 2:
+            self.__dword = self.__raw_value_word
 
-            if type == 3:
-                self.__char_last = self.__binary[26:]
-                self.__char_last = chr(int(self.__char_last, 2))
+        if type == 3:
+            self.__char_last = self.__binary[26:]
+            self.__char_last = chr(int(self.__char_last, 2))
 
-                self.__char_third = self.__binary[18:26]
-                self.__char_third = chr(int(self.__char_third, 2))
+            self.__char_third = self.__binary[18:26]
+            self.__char_third = chr(int(self.__char_third, 2))
 
-                self.__char_second = self.__binary[10:18]
-                self.__char_second = chr(int(self.__char_second, 2))
+            self.__char_second = self.__binary[10:18]
+            self.__char_second = chr(int(self.__char_second, 2))
 
-                self.__char_first = self.__binary[:10]
-                self.__char_first = chr(int(self.__char_first, 2))
+            self.__char_first = self.__binary[:10]
+            self.__char_first = chr(int(self.__char_first, 2))
 
-                self.__dword = self.__char_first + self.__char_second + self.__char_third + self.__char_last
+            self.__dword = self.__char_first + self.__char_second + \
+                self.__char_third + self.__char_last
 
-            return self.__dword
+        return self.__dword
