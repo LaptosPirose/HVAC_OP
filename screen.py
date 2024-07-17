a4273796116ffb9f2cb7e03c0a65c51180580ea1
Angelo <angelomoura@gmail.com>
2024-07-17 15:20:51 -0300

    calculate feet

--- screen.py
+++ screen.py
@@ -20,13 +20,45 @@ window_pos_y = int((screen_height/2) - (window_height/2))
 root.geometry(str(window_width)+'x'+str(window_height) +
               '+'+str(window_pos_x)+'+'+str(window_pos_y))
 
-root.resizable(False, False)
+root.resizable(True, True)
+root.columnconfigure(0, weight=1)
 
 root.title("Distance Converter")
+
+meters_value = tk.StringVar()
+
+
+def calculate_feet(*args):
+    try:
+        pass
+        meters = float(meters_value.get())
+        feet = meters * 3.28084
+        print(f'{meters} meters is equal to {feet:.3f} feet.')
+    except ValueError:
+        feet_display.config(text='Invalid input')
+        return
+
 
 main = ttk.Frame(
     root,
     padding=(30, 15)
 )
+main.grid()
+
+meter_label = ttk.Label(master=main, text='Meters:')
+meter_input = ttk.Entry(master=main, width=10, textvariable=meters_value)
+
+feet_label = ttk.Label(master=main, text='Feet:')
+feet_display = ttk.Label(master=main, text='Feet from here')
+calc_button = ttk.Button(master=main, text='Calculate', command=calculate_feet)
+
+meter_label.grid(row=0, column=0, sticky='W', padx=5, pady=5)
+meter_input.grid(row=0, column=1, sticky='EW', padx=5, pady=5)
+meter_input.focus()
+
+feet_label.grid(row=1, column=0, sticky='W', padx=5, pady=5)
+feet_display.grid(row=1, column=1, sticky='EW', padx=5, pady=5)
+
+calc_button.grid(row=2, column=0, columnspan=2, sticky='EW', padx=5, pady=5)
 
 root.mainloop()
