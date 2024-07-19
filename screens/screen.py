3f1062395be2c0b5ac6054056398209b067c28d9
Angelo <angelomoura@gmail.com>
2024-07-19 10:21:38 -0300

    both calculation

--- screens/screen.py
+++ screens/screen.py
@@ -12,6 +12,12 @@ set_dpi_awareness.set_dpi_awareness()
 
 
 class DistanceConverter(tk.Tk):
+    """
+        Main window for the distance converter app.
+
+    Args:
+        tk (_type_): tkinter min window
+    """
 
     def __init__(self, width, height, x_pos, y_pos, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,14 +31,57 @@ class DistanceConverter(tk.Tk):
 
         self.geometry(self.width+'x'+self.height+'+'+self.x_pos+'+'+self.y_pos)
 
-        self.frame = AppFrame(self)
+        self.frame = MetersToFeet(self)
         self.frame.grid()
 
         self.bind('<Return>', self.frame.calculate_feet)
         self.bind('<KP_Enter>', self.frame.calculate_feet)
 
 
-class AppFrame(ttk.Frame):
+class MetersToFeet(ttk.Frame):
+
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
+        self['padding'] = (30, 30)
+
+        self.meters_value = tk.StringVar()
+        self.feet_value = tk.StringVar()
+
+        self.meter_label = ttk.Label(master=self, text='Meters:')
+        self.meter_input = ttk.Entry(master=self, width=10,
+                                     textvariable=self.meters_value)
+        self.meter_input.config(font=('Segoe UI', 10))
+
+        self.feet_label = ttk.Label(master=self, text='Feet:')
+        self.feet_display = ttk.Label(master=self, text='Feet from here')
+        self.calc_button = ttk.Button(
+            master=self, text='Calculate', command=self.calculate_feet)
+
+        self.meter_label.grid(row=0, column=0, sticky='W')
+        self.meter_input.grid(row=0, column=1, sticky='EW')
+        self.meter_input.focus()
+
+        self.feet_label.grid(row=1, column=0, sticky='W')
+        self.feet_display.grid(row=1, column=1, sticky='EW')
+
+        self.calc_button.grid(row=2, column=0, columnspan=2, sticky='EW')
+
+        for self.child in self.winfo_children():
+            self.child.grid_configure(padx=5, pady=5)
+
+    def calculate_feet(self, *args):
+
+        try:
+            self.meters = float(self.meters_value.get())
+            self.feet = self.meters * 3.28084
+            print(f'{self.meters} meters is equal to {self.feet:.3f} feet.')
+            self.feet_value.set(f'{self.feet:.3f}')
+            self.feet_display.config(text=f'{self.feet_value.get()}')
+        except ValueError:
+            self.feet_display.config(text='Invalid input')
+        return
+    
+class MetersToFeet(ttk.Frame):
 
     def __init__(self, container, **kwargs):
         super().__init__(container, **kwargs)
