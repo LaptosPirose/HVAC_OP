76c7d8b1650c1ec3be8a707d3e69e4a0547d19f0
Angelo <angelomoura@gmail.com>
2024-08-14 11:01:58 -0300

    events

--- PyQt6/Qt6_OO/OO_event_handling.py
+++ PyQt6/Qt6_OO/OO_event_handling.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout
 from PyQt6.QtGui import QIcon, QFont, QMovie, QPixmap
 
 import sys
@@ -17,12 +17,26 @@ class Widget(QWidget):
         self.setWindowTitle("Python GUI Development - Event Handling")
         self.setGeometry(100, 100, 1200, 700)
 
-        self.setWindowIcon(QIcon(path + 'icon.png'))
+        self.setWindowIcon(QIcon(path + '5968350.png'))
 
         qlabel = QLabel(self)
         qlabel.setText(f"Path added to project: {path}")
         qlabel.setFont(QFont('Times New Roman', 18))
         qlabel.setStyleSheet("color: red")
+
+        self.create_widget()
+
+    def create_widget(self):
+        hbox = QHBoxLayout()
+        btn1 = QPushButton('click Me To change Text')
+        self.label1 = QLabel("Default Text")
+
+        hbox.addWidget(btn1)
+        hbox.addWidget(self.label1)
+        self.setLayout(hbox)
+
+    def clicked_event(self):
+        self.label1.setText('Button Clicked!')
 
 
 app = QApplication(sys.argv)
