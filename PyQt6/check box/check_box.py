ebad3c49649c07706c3500191d1155958ae8f3d0
Angelo <angelomoura@gmail.com>
2024-08-15 11:30:45 -0300

    Checkbox

--- PyQt6/check box/check_box.py
+++ PyQt6/check box/check_box.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout, QMenu
 from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtCore import QSize
 import sys
@@ -10,10 +10,29 @@ class Frame(QWidget):
         self.setWindowTitle("Python GUI Development -  QCheckBox")
         self.setGeometry(200, 200, 800, 580)
         self.setWindowIcon(QIcon('PyQt6/images/5968350.png'))
-        
-        self.create_radio_button()
-        
-    
+
+        menu = QMenu("Python")
+
+        self.check1 = QCheckBox(self)
+        self.check1.setText("Option 1")
+        self.check1.setIcon(QIcon('PyQt6/images/icon.png'))
+        self.check1.setIconSize(QSize(60, 60))
+
+        self.check2 = QCheckBox(self)
+        self.check2.setText("Option 2")
+        self.check2.setIcon(QIcon('PyQt6/images/python.png'))
+        self.check2.setIconSize(QSize(60, 60))
+
+        self.check3 = QCheckBox(self)
+        self.check3.setText("Option 3")
+
+        self.hbox = QHBoxLayout(self)
+        self.hbox.addWidget(self.check1)
+        self.hbox.addWidget(self.check2)
+        self.hbox.addWidget(self.check3)
+
+        self.setLayout(self.hbox)
+
 
 app = QApplication(sys.argv)
 window = Frame()
